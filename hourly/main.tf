9100e2d30200ee0b53707139c2da52f5dca40978
taher <taherbohari52@gmail.com>
2021-03-03 07:53:43 +0000

    manage terraform version at root level

--- hourly/main.tf
+++ hourly/main.tf
@@ -1,13 +1,3 @@
-terraform {
-  required_version = ">= 0.13.0"
-  required_providers {
-    aws = {
-      source  = "hashicorp/aws"
-      version = "~> 3.0"
-    }
-  }
-}
-
 provider "aws" {
   region                  = "us-west-2"
   shared_credentials_file = "/home/taher/.aws/credentials"
--- modules/s3/main.tf
+++ modules/s3/main.tf
@@ -1,3 +1,13 @@
+terraform {
+  required_version = ">= 0.13.0"
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 3.0"
+    }
+  }
+}
+
 resource "aws_s3_bucket" "friday" {
   count = var.create_bucket ? 1 : 0
 
